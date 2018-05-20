06242c8350ad852cb75853be762e794339560e9c
Billy <billyinthe510@yahoo.com>
2018-05-20 12:06:48 -0700

    Flatflex In Progress, Working on Timing FlatBuffer of FlexBuffers

--- flatflex.cpp
+++ flatflex.cpp
@@ -6,12 +6,12 @@
 #include <fstream>
 #include <sstream>
 #include <sys/time.h>
-#include "lineitem_generated.h"
+#include "flatbuffers/flexbuffers.h"
+#include "flatflex_generated.h"
 
 using namespace std;
 using namespace Tables;
 
-void LINEITEM_add_fields(); // THIS IS UNUSED BUT HERE FOR INCASE NEEDED
 std::vector<std::string> split(const std::string &s, char delim);
 std::vector<int> splitDate(const std::string &s);
 
@@ -29,13 +29,14 @@ int main()
 	// Get a row
 	std::string row;
 	std::getline(inFile, row);
-	inFile.close();
+	//inFile.close();
 	// Split by '|' deliminator
 	std::vector<std::string> parsedRow = split(row, '|');
 	
 // ------------------------------------------------------------------------Initialize FlatBuffer ----------------------------------------
-	//	 Create a 'FlatBufferBuilder', which will be used to create our LINEITEM FlatBuffers
+	//	 Create a 'FlatBufferBuilder', which will be used to create our LINEITEM FlexBuffers
 	flatbuffers::FlatBufferBuilder builder(1024);
+	vector<flatbuffers::Offset<Rows>> rows_vector;
 	//	 Creating temp variables to throw into the builder
 	int orderkey = atoi(parsedRow[0].c_str());
 	int partkey = atoi(parsedRow[1].c_str());
@@ -49,35 +50,146 @@ int main()
 	int8_t returnflag = (int8_t) atoi(parsedRow[8].c_str());
 	int8_t linestatus = (int8_t) atoi(parsedRow[9].c_str());
 	//	 Parsing and Serializing of Date fields	
-	vector<int> s,r,c;
-	s = splitDate(parsedRow[10]);
-	r = splitDate(parsedRow[11]);
-	c = splitDate(parsedRow[12]);
-	auto shipdate = Date(s[0], s[1], s[2]);
-	auto commitdate = Date(r[0], r[1], r[2]);
-	auto receiptdate = Date(c[0], c[1], c[2]);
-	//	 Serializing String fields
-	auto shipinstruct = builder.CreateString(parsedRow[13]);
-	auto shipmode = builder.CreateString(parsedRow[14]);
-	auto comment = builder.CreateString(parsedRow[15]);
-	// Initialize our LINEITEM
-	auto firstItem = CreateLINEITEM(builder, orderkey, partkey, suppkey, linenumber, // int32_t, 4 byte int
-				quantity, extendedprice,discount,tax, // float
-				returnflag,linestatus, // char as an int8_t
-				&shipdate,&commitdate,&receiptdate, // serialized Date struct
-				shipinstruct,shipmode,comment); // serialized string type
+	vector<int> shipdate, receiptdate, commitdate;
+	shipdate = splitDate(parsedRow[10]);
+	receiptdate = splitDate(parsedRow[11]);
+	commitdate = splitDate(parsedRow[12]);
+	string shipinstruct = parsedRow[13];
+	string shipmode = parsedRow[14];
+	string comment = parsedRow[15];
 
-	builder.Finish(firstItem);
+	int rowID = 0;
+// -----------------------------------------Create Row 0-------------------------------------------
+	flexbuffers::Builder fbb0;
+	fbb0.Vector([&]() {
+		fbb0.UInt(orderkey);
+		fbb0.UInt(partkey);
+		fbb0.UInt(suppkey);
+		fbb0.UInt(linenumber);
+		fbb0.Float(quantity);
+		fbb0.Float(extendedprice);
+		fbb0.Float(discount);
+		fbb0.Float(tax);
+		fbb0.UInt(returnflag);
+		fbb0.UInt(linestatus);
+		fbb0.Vector([&]() {
+			fbb0.UInt(shipdate[0]);
+			fbb0.UInt(shipdate[1]);
+			fbb0.UInt(shipdate[2]);
+		});
+		fbb0.Vector([&]() {
+			fbb0.UInt(receiptdate[0]);
+			fbb0.UInt(receiptdate[1]);
+			fbb0.UInt(receiptdate[2]);
+		});
+		fbb0.Vector([&]() {
+			fbb0.UInt(commitdate[0]);
+			fbb0.UInt(commitdate[1]);
+			fbb0.UInt(commitdate[2]);
+		});
+		fbb0.String(shipinstruct);
+		fbb0.String(shipmode);
+		fbb0.String(comment);
+	});
+	fbb0.Finish();
+	// Get Pointer to FlexBuffer Row
+	vector<uint8_t> buff = fbb0.GetBuffer();
+	// Serialize buffer into a FlatBuffer::Vector
+	auto fb0 = builder.CreateVector(buff);
+	// Create a Row from FlexBuffer and new ID
+	auto row0 = CreateRows(builder, rowID++, fb0); 
+	// Push new row onto vector of rows
+	rows_vector.push_back(row0);	
+// --------------------------------------------------Create Row 1-------------------------------------------------
+	// Get a row
+	getline(inFile, row);
+	// Split by '|' deliminator
+	parsedRow = split(row, '|');
+	orderkey = atoi(parsedRow[0].c_str());
+	partkey = atoi(parsedRow[1].c_str());
+	suppkey = atoi(parsedRow[2].c_str());
+	linenumber = atoi(parsedRow[3].c_str());
+	quantity = stof(parsedRow[4]);
+	extendedprice = stof(parsedRow[5]);
+	discount = stof(parsedRow[6]);
+	tax = stof(parsedRow[7]);
+	
+	returnflag = (int8_t) atoi(parsedRow[8].c_str());
+	linestatus = (int8_t) atoi(parsedRow[9].c_str());
+	//	 Parsing and Serializing of Date fields	
+	shipdate = splitDate(parsedRow[10]);
+	receiptdate = splitDate(parsedRow[11]);
+	commitdate = splitDate(parsedRow[12]);
+	shipinstruct = parsedRow[13];
+	shipmode = parsedRow[14];
+	comment = parsedRow[15];
+
+	flexbuffers::Builder fbb1;
+	fbb1.Vector([&]() {
+		fbb1.UInt(orderkey);
+		fbb1.UInt(partkey);
+		fbb1.UInt(suppkey);
+		fbb1.UInt(linenumber);
+		fbb1.Float(quantity);
+		fbb1.Float(extendedprice);
+		fbb1.Float(discount);
+		fbb1.Float(tax);
+		fbb1.UInt(returnflag);
+		fbb1.UInt(linestatus);
+		fbb1.Vector([&]() {
+			fbb1.UInt(shipdate[0]);
+			fbb1.UInt(shipdate[1]);
+			fbb1.UInt(shipdate[2]);
+		});
+		fbb1.Vector([&]() {
+			fbb1.UInt(receiptdate[0]);
+			fbb1.UInt(receiptdate[1]);
+			fbb1.UInt(receiptdate[2]);
+		});
+		fbb1.Vector([&]() {
+			fbb1.UInt(commitdate[0]);
+			fbb1.UInt(commitdate[1]);
+			fbb1.UInt(commitdate[2]);
+		});
+		fbb1.String(shipinstruct);
+		fbb1.String(shipmode);
+		fbb1.String(comment);
+	});
+	fbb1.Finish();
+	// Get Pointer to FlexBuffer Row
+	buff = fbb1.GetBuffer();
+	// Serialize buffer into a FlatBuffer::Vector
+	auto fb1 = builder.CreateVector(buff);
+	// Create a Row from FlexBuffer and new ID
+	auto row1 = CreateRows(builder, rowID++, fb1); 
+	// Push new row onto vector of rows
+	rows_vector.push_back(row1);	
+//--------------------------------------------------------------Created 2 rows -------------------------------------
+	auto table = CreateTable(builder, rows_vector);
+	builder.Finish(table);
+
+	uint8_t *buf = builder.GetBufferPointer();
+	int size = builder.GetSize();
+	cout<<"Buffer Size: "<<size<<endl<<endl;
+
+	auto records = GetTable(buf);
+	flatbuffers::Vector<flatbuffers::Offset<Rows>> recs = records->data();
+	auto firstRow = flexbuffers::GetRoot(recs[0]).AsVector();
+	cout<<"First Row's Orderkey: "<<firstRow[0]<<endl;
+
+	auto record = flexbuffers::GetRoot(buff).AsUInt8();
+	
+/*
 // --------------------------------------------Start Timing Rows--------------------------------------------------------------------
 //
 	struct timeval start, end;
 	double t;
 
-	uint8_t *buf = builder.GetBufferPointer();
-	int size = builder.GetSize();
-	std::cout<<"Buffer Size (FlatBuffer): "<<size<<endl<<endl;
+//	uint8_t *buf = builder.GetBufferPointer();
+//	int size = builder.GetSize();
+//	std::cout<<"Buffer Size (FlatBuffer): "<<size<<endl<<endl;
 
-	volatile auto item = GetLINEITEM(buf);
+//	volatile auto item = GetLINEITEM(buf);
 
 	volatile int32_t _orderkey, _partkey, _suppkey, _linenumber;
 	volatile float _quantity, _extendedprice, _discount, _tax;
@@ -259,7 +371,7 @@ int main()
 
 	assert(_returnflag == returnflag);
 	assert(_linestatus == linestatus);
-
+*/
 
 	return 0;
 }
@@ -281,39 +393,4 @@ std::vector<int> splitDate(const std::string &s) {
 		tokens.push_back(atoi(item.c_str()));
 	}
 	return tokens;
-}
-void LINEITEM_add_fields() {
-	
-	flatbuffers::FlatBufferBuilder builder(1024);
-	/*
-	// Initialize
-	LINEITEMBuilder lineitem_builder(builder);
-	lineitem_builder.add_L_ORDERKEY(parsedRow[0]);
-	lineitem_builder.add_L_PARTKEY(parsedRow[1]);
-	lineitem_builder.add_L_SUPPKEY(parsedRow[2]);
-	lineitem_builder.add_L_LINENUMBER(parsedRow[3]);
-	lineitem_builder.add_L_QUANTITY(parsedRow[4]);
-	lineitem_builder.add_L_EXTENDEDPRICE(parsedRow[5]);
-	lineitem_builder.add_L_DISCOUNT(parsedRow[6]);
-	lineitem_builder.add_L_TAX(parsedRow[7]);
-
-	lineitem_builder.add_L_RETURNFLAG(parsedRow[8]);
-	lineitem_builder.add_L_LINESTATUS(parsedRow[9]);
-
-	lineitem_builder.add_L_SHIPDATE(parsedRow[10]);
-	lineitem_builder.add_L_COMMITDATE(parsedRow[11]);
-	lineitem_builder.add_L_RECEIPTDATE(parsedRow[12]);
-	lineitem_builder.add_L_SHIPINSTRUCT(parsedRow[13]);
-	lineitem_builder.add_L_SHIPMODE(parsedRow[14]);
-	lineitem_builder.add_L_COMMENT(parsedRow[15]);
-	// If we do not wish to set every field in a table
-	// Use this instead of calling CreateLINEITEM()
-	auto firstItem = lineitem_builder.Finish();
-	
-
-	// Call 'Finish()' to instruct the builder that this LINEITEM Is complete
-	// Regardless of how 'firstItem' was created, we still need to call
-	// 'Finish()' on the 'FlatBufferBuilder'
-	builder.Finish(firstItem); // Could also call 'FinishLINEITEMBuffer(builder, firstItem);
-	*/
 }
